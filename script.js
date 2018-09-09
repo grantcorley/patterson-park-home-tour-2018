22531c0f96ce577ed6dd7b166e2564b872a040af
Grant Corley <grantcorley@gmail.com>
2018-09-08 21:35:25 -0400

    added images sizing js and rotation animation css to main img

--- script.js
+++ script.js
@@ -1,23 +1,110 @@
 console.log('js is ready');
 
-//let globalWidth;
+
+const image = document.getElementsByTagName("main")[0];
+const imageScale = 1.85;
+let imageSize;
+let topMargin;
+let leftMargin;
 
 
 function checkDimensions(){
-    console.log("checkDimensions()");
+    //console.log("checkDimensions()");
     const globalWidth = document.body.clientWidth;
     const globalHeight = document.body.clientHeight;
+    //const topMargin;
     
-    console.log(`the doc width = ${globalWidth}` );
-    console.log(`the doc height = ${globalHeight}` );
+    // console.log(`the doc width = ${globalWidth}` );
+    // console.log(`the doc height = ${globalHeight}` );
+
+    if(globalWidth < globalHeight){
+        imageSize = (globalWidth*imageScale);
+    } else if(globalHeight < globalWidth){
+        imageSize = (globalHeight*imageScale);
+    } else if(globalHeight === globalWidth){
+        imageSize = (globalWidth*imageScale);
+    }
+
+    //console.log(`imageSize = ${imageSize}`);
+
+   topMargin = getTopMargin(imageSize, globalHeight);
+   leftMargin = getLeftMargin(imageSize, globalWidth);
+
+
+    console.log(`imageSize = ${imageSize}` );
+
+    image.style.width = (imageSize) + "px";
+    image.style.height = (imageSize) + "px";
+    image.style.marginTop = topMargin + "px";
+    image.style.marginLeft = leftMargin + "px";
 
 }
+
+
+
+function getLeftMargin(imageSize, globalWidth){
+    console.log(`getLeftMargin(${imageSize}, ${globalWidth})`);
+    
+    if(imageSize > globalWidth){
+
+        //console.log("CONDITION MET!!!!");
+        var margLeft = (0 - (imageSize - globalWidth)/2);
+        console.log(`margLeft = ${margLeft}`);
+        return margLeft;
+
+    }
+
+    else if(globalWidth > imageSize){
+
+        margLeft = (globalWidth - imageSize)/2;
+        return margLeft;
+
+    }
+
+    return 0;
+
+}
+
+
+function getTopMargin(imageSize, globalHeight){
+    console.log(`getTopMargin(${imageSize}, ${globalHeight})`);
+    
+    let margTop;
+
+    if(imageSize > globalHeight){
+
+        //console.log("CONDITION MET!!!!");
+        margTop = (0 - (imageSize - globalHeight)/2);
+        console.log(`margTop = ${margTop}`);
+        return margTop;
+
+    } else if(globalHeight > imageSize){
+
+        margTop = (globalHeight - imageSize)/2;
+        return margTop;
+
+    } else return 0;
+
+}
+
+
+
 
 
 
 //when program starts running...
 checkDimensions();
 
+
+//if browser gets resized...
 window.addEventListener("resize", checkDimensions);
 
 
+// var theInterval = setInterval(myTimer, 100);
+
+// function myTimer() {
+    
+//     console.log("myTimer()");
+//     image.style.transform = 'rotate(1deg)';
+
+// }
